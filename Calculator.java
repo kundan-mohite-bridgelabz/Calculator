0a981189a5218c08ba66f2e8885b5ad33689bfc0
Kundan Mohite <kundanmohite11@gmail.com>
2025-08-06 10:20:47 +0530

    Implemented add method

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,11 @@
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Hellow World");
+        Calculator obj = new Calculator();
+        obj.add(5,10);
+    }
+
+    public void add(int a, int b){
+        System.out.println(a+b);
     }
 }
